2ee9e97001bde783c0520d80c331932c05cad709
gracetrannn <tranthanhthaotrang@gmail.com>
2024-10-24 15:12:03 +0700

    Change the style in CSS

--- stock-market-frontend/src/App.js
+++ stock-market-frontend/src/App.js
@@ -32,20 +32,45 @@ const Stocks = ({ addToWatchlist }) => {
 			<h1>Stock Market MERN App</h1>
 			<h2>Stocks</h2>
 			<ul>
+				<li style={{ background: '#EEEEEE', color: '#000000' }}>
+					<div style={{ width: '20%' }}>
+						company
+					</div>
+					<div style={{ width: '20%' }}>
+						initial_price
+					</div>
+					<div style={{ width: '20%' }}>
+						price_2002
+					</div>
+					<div style={{ width: '20%' }}>
+						price_2007
+					</div>
+					<div style={{ width: '20%' }}>
+						Add Button
+					</div>
+				</li>
 				{stocks.map((stock) => (
 					<li key={stock.symbol}>
-						{stock.company} ({stock.symbol}) -
-						<span style={{ color: getRandomColor() }}>
+						<div style={{ width: '20%' }}>{stock.company} ({stock.symbol}) -</div>
+						<div style={{ width: '20%' }}><span style={{ color: getRandomColor() }}>
 							{" "}
 							${stock.initial_price}
-						</span>
-						<button onClick={() => addToWatchlist(stock)}>
+						</span></div>
+						<div style={{ width: '20%' }}><span style={{ color: getRandomColor() }}>
+							{" "}
+							${stock.price_2002}("2002")
+						</span></div>
+						<div style={{ width: '20%' }}><span style={{ color: getRandomColor() }}>
+							{" "}
+							${stock.price_2007}("2007")
+						</span></div>
+						<div style={{ width: '20%' }}><button onClick={() => addToWatchlist(stock)}>
 							Add to My Watchlist
-						</button>
+						</button></div>
 					</li>
 				))}
 			</ul>
-		</div>
+		</div >
 	);
 };
 
@@ -124,24 +149,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
